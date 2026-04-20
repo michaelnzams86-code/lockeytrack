9e7f0c9763b0dbf57ae9eb0f55a4af78f605a20f
michaelnzams86-code <michaelnzams86@gmail.com>
2026-04-21 00:12:47 +0300

    Enable serving static files in server.js
    Added middleware to serve static files from the root directory.

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the static HTML file
+// This line tells the server to allow access to all files in the folder
+app.use(express.static(path.join(__dirname, '/')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is live on port ${PORT}`);
 });
